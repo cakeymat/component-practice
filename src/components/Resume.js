38ef88e67d0f974b997aecbe47eef762dad6930b
Vincent <xvincentcosciax@gmail.com>
2019-03-28 12:47:42 -0400

    initial commit

---
+++ src/components/Resume.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Resume extends Component{
+    render (){
+        return(
+            <div id="resume">
+              <h2>Resume</h2>
+                <header>
+                <h1>Vincent Coscia</h1>
+                <h2>Address Line 1</h2>
+                <h2>Address Line 2</h2>
+                </header>
+            </div>
+        )
+    }
+}
+
+
+export default Resume;
